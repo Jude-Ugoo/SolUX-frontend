b073fc4572a0e828dfa3c3fafd7671b5a562ed55
Jonathan Filippini <filippini.jo@gmail.com>
2025-05-15 17:17:45 +0200

    feat: chat component with html render

--- src/app/api/generate-ai/route.js
+++ src/app/api/generate-ai/route.js
@@ -1,6 +1,7 @@
 import { deepseek } from '@ai-sdk/deepseek';
 import { generateText } from 'ai';
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+import { saveChat } from '@/app/tools/chat-store';
 
 
 // Allow streaming responses up to 30 seconds
@@ -13,22 +14,36 @@ const openrouter = createOpenRouter({
 
 
 export async function POST(req) {
-  const { messages } = await req.json();
+  let { messages, userId } = await req.json();
   console.log('Server: Received messages for DeepSeek API:', messages);
+  if (messages.length <= 2) {
+    messages.push({ role: 'system', content: "You are an expert UI developer. Generate a complete HTML page based on the user's prompt. Return only the HTML code." });
+  }
 
   try {
     // const model = openrouter.chat('meta-llama/llama-3.1-405b-instruct')
     // const model = openrouter.chat('deepseek/deepseek-prover-v2:free')
-    const model = deepseek('deepseek-chat'); // This could potentially throw if model init fails
+    const model = openrouter.chat('google/gemini-2.5-pro-preview')
+    // const model = deepseek('deepseek-chat'); // This could potentially throw if model init fails
 
     const { text } = await generateText({
       model: model,
-      // system:
-      // `You are an expert UI developer. Generate a complete HTML page based on the user's prompt. Return only the HTML code.`,
       messages,
     });
-    console.log('text?????', text);
-    return new Response(JSON.stringify({ text }));
+    // await saveChat({
+    //   userId,
+    //   createdAt: new Date(),
+    //   message: messages[messages.length - 1],
+    //   output: text
+    // });
+    console.log('text>>>>', text);
+    console.log('output messages>>>>', messages);
+    return new Response(JSON.stringify({messages: [{
+      role: 'assistant',
+      content: text
+    }]}), {
+      headers: { 'Content-Type': 'application/json' }
+    });
   } catch (e) {
     console.error('Server: Error in POST /api/deepseek (outer catch block):', e);
     // Return a proper error response to the client
